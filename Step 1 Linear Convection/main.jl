db9e44e1489b018ac8d8455f4b48d9c701790b2e
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-03-23 15:46:58 -0600

    Add Step1 and 2

---
+++ Step 1 Linear Convection/main.jl
@@ -0,0 +1,36 @@
+using Plots
+
+
+function Wave(u::Array{Float64})
+const nx = 41
+const dx = 2 / (nx-1)
+const nt = 25
+const dt = 0.025
+const c = 1
+
+u::Arrau{Float64} = Array{Float64}(undef , nx);
+u = ones(nx)
+
+for i in range(Int(round(nx/3)), stop=Int(round(2*nx/3)))
+
+    u[i] = 2;
+
+end
+
+pb = plot(u)
+
+for i in range(1, stop=nt)
+    u_old = Array{Float64}(undef, nx)
+    u_old = copy(u)
+    for j in range(2, stop=nx)
+
+        u[j] = u_old[j] - c * dt / dx * (u_old[j]- u_old[j-1])
+
+    end
+    
+end
+
+pa = plot(u)
+
+plot(pb, pa, layout=(2,1))
+end
