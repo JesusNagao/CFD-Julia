18b4be383d80ae1bd6ffd2ff1b22fb3a2c77e638
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-03-24 23:33:49 -0600

    Update

--- Step 5 2D Linear Convection/time.jl
+++ Step 5 2D Linear Convection/time.jl
@@ -4,10 +4,10 @@ using Plots;
 function time_iteration(u::Matrix{Float64}, un::Matrix{Float64}, nt::Int64, c::Int64, dt::Float64, dx::Float64, dy::Float64)
     
     @gif for n in range(1,stop=nt+1)
-        un = u
-        xlims!(0, 2)
-        ylims!(0,2)
-        zlims!(0,2)
+        un = u;
+        xlims!(0.0, 2.0)
+        ylims!(0.0,2.0)
+        zlims!(0.0,2.0)
         surface(x, y, u)
         space_iteration(u, un, c, dt, dx, dy)
     end
