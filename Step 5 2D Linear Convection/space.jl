68d577d6a80d0bf0ac63c0bb38ffa9ec5fdf5585
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-03-25 09:23:42 -0600

    U

--- Step 5 2D Linear Convection/space.jl
+++ Step 5 2D Linear Convection/space.jl
@@ -5,10 +5,6 @@ function space_iteration(u::Matrix{Float64}, un::Matrix{Float64}, c::Int64, dt::
     @threads for i in range(2, stop=nx)
         @threads for j in range(2, stop=ny)
             u[i, j] = (un[i, j] - (c * dt / dx * (un[i, j] - un[i, j-1]))-(c * dt / dy * (un[i, j] - un[i-1, j])))
-            #u[2, :] .= 1
-            #u[-1, :] .= 1
-            #u[:,1] .= 1
-            #u[:, -1] .= 1
         end
     end
 end
