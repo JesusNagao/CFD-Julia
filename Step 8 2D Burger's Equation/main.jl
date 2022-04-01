3c6102f3b0fecbb88de5e094495309c6f16ffea8
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-03-31 19:07:23 -0600

    update'

--- Step 8 2D Burger's Equation/main.jl
+++ Step 8 2D Burger's Equation/main.jl
@@ -1,1 +1,25 @@
+const nx = 41
+const ny = 41
+const nt = 120
+const c = 1
+const dx = 2/(nx-1)
+const dy = 2/(ny-1)
+const sigma = 0.0009
+const nu = 0.01
+const dt = sigma * dx * dy / nu
+
+x = Array{Float64}([i for i in range(0, stop=2, step=dx)])
+y = Array{Float64}([i for i in range(0, stop=2, step=dy)])
+
+u = Matrix{Float64}(undef, nx, ny)
+u = zeros(nx, ny)
+v = Matrix{Float64}(undef, nx, ny)
+v = zeros(nx, ny)
+
+un = Matrix{Float64}(undef, nx, ny)
+un = zeros(nx, ny)
+vn = Matrix{Float64}(undef, nx, ny)
+vn = zeros(nx, ny)
+comb = Matrix{Float64}(undef, nx, ny)
+comb = zeros(nx, ny)
 
