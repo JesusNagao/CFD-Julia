3a0cc1da11a27c136bf40eb3209753ea97b822b8
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-04-05 02:59:57 -0500

    Update

--- Step 12 Channel Flow/main.jl
+++ Step 12 Channel Flow/main.jl
@@ -6,3 +6,11 @@ const nit = 51
 const c = 1
 const dx = 2 / (nx - 1)
 const dy = 2 / (ny - 1)
+
+x = Array{Float64}([i for i in range(0.0, stop=2.0, step=dx)])
+y = Array{Float64}([i for i in range(0.0, stop=2.0, step=dy)])
+
+const rho = 1
+const nu = 0.1
+const F = 1
+const dt = 0.01
