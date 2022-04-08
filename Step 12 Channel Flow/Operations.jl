3d3acd6ae0254879a79f7deb344da05b0955ea1d
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-04-07 22:58:36 -0500

    Update

--- Step 12 Channel Flow/Operations.jl
+++ Step 12 Channel Flow/Operations.jl
@@ -7,3 +7,9 @@ function build_up_b(rho::Int64, dt::Float64, dx::Float64, dy::Float64, u::Matrix
     b[2:nx-1, 1] = (rho * (1 / dt * ((u[2:nx-1, 2] - u[2:nx-1, ny-1]) / (2 * dx) + (v[3:nx, 1] - v[1:nx-2, 1]) / (2 * dy) - ((u[2:nx-1, 2] - u[2:nx-1, ny-1]) / (2 * dx)) .^ 2 - 2 * ((u[3:nx, 1] - u[1:nx-2, ny-1]) / (2*dy) .* (v[2:nx-1, 1] - v[2:nx-1, ny-1]) / (2 * dx)) - ((v[3:nx, 1] - v[1:nx-2, 1]) / (2 * dy)).^2)))
     
 end
+
+function pressure_poisson_periodic()
+
+
+
+end
