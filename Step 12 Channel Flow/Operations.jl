6c95748ae80d69d6a46b776c9f65916a25a06871
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-04-08 01:13:42 -0500

    Update

--- Step 12 Channel Flow/Operations.jl
+++ Step 12 Channel Flow/Operations.jl
@@ -8,8 +8,19 @@ function build_up_b(rho::Int64, dt::Float64, dx::Float64, dy::Float64, u::Matrix
     
 end
 
-function pressure_poisson_periodic()
+function pressure_poisson_periodic(p::Matrix{Float64}, dx::Float64, dy::Float64, nit::Int64, nx::Int64, ny::Int64)
 
+    for j in range(1, stop=nit)
+    
+        pn = Matrix{Float64}(undef, nx, ny)
+        pn = p
 
+        p[2:nx-1, 2:ny-1] = (((pn[2:nx-1, 3:ny] + pn[2:nx-1, 1:ny-2]) * dy^2 + (pn[3:nx, 2:ny-1] + pn[1:nx-2, 2:ny-1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, 2:nx-1])
+        p[2:nx-1, ny-1] = (((pn[2:nx-1, 1] + pn[2:nx-1, ny-2])* dy^2 + (pn[3:nx, ny-1] + pn[1:nx-2, ny-1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, ny-1])
+        p[2:nx-1, 1] = (((pn[2:nx-1, 2] + pn[2:nx-1, ny-1])* dy^2 + (pn[3:nx, 1] + pn[1:ny-2, 1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, 1])
+        p[nx-1, :] =p[nx-2, :]
+        p[1, :] = p[2, :]
+
+    end
 
 end
