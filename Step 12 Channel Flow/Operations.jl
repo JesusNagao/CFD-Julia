3a92e6d622efd44bbf35f4779962621ef01fff6b
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-04-28 10:28:50 -0500

    redo

--- Step 12 Channel Flow/Operations.jl
+++ Step 12 Channel Flow/Operations.jl
@@ -1,32 +1,12 @@
-#using GLMakie
+function pressure_equation()
 
-function build_up_b(rho::Int64, dt::Float64, dx::Float64, dy::Float64, u::Matrix{Float64}, v::Matrix{Float64}, b::Matrix{Float64}, nx::Int64, ny::Int64)
+    for i in range(1, stop=nx)
+        for j in range(1, stop=ny)
 
-    b[2:nx-1,2:ny-1] = (rho * (1 / dt * ((u[2:nx-1, 3:ny] - u[2:nx-1, 1:ny-2]) / (2 * dx) + (v[3:nx, 2:ny-1] - v[1:nx-2, 2:ny-1]) / (2 * dy) - ((u[2:nx-1, 3:ny] - u[2:nx-1, 1:ny-2]) / (2 * dx)) ^ 2 - 2 * ((u[3:nx, 2:ny-1] - u[1:nx-2, 2:ny-1]) / (2*dy) * (v[2:nx-1, 3:ny] - v[2:nx-1, 1:ny-2]) / (2 * dx)) - ((v[3:nx, 2:ny-1] - v[1:nx-2, 2:ny-1]) / (2 * dy))^2)))
-    b[2:nx-1,ny-1] = (rho * (1 / dt * ((u[2:nx-1, 1] - u[2:nx-1, ny-2]) / (2 * dx) + (v[3:nx, ny-1] - v[1:nx-2, ny-1]) / (2 * dy) - ((u[2:nx-1, 1] - u[2:nx-1, ny-2]) / (2 * dx)) .^ 2 - 2 * ((u[3:nx, ny-1] - u[1:nx-2, ny-1]) / (2*dy) .* (v[2:nx-1, 1] - v[2:nx-1, ny-2]) / (2 * dx)) - ((v[3:nx, ny-1] - v[1:nx-2, ny-1]) / (2 * dy)).^2)))
-    b[2:nx-1, 1] = (rho * (1 / dt * ((u[2:nx-1, 2] - u[2:nx-1, ny-1]) / (2 * dx) + (v[3:nx, 1] - v[1:nx-2, 1]) / (2 * dy) - ((u[2:nx-1, 2] - u[2:nx-1, ny-1]) / (2 * dx)) .^ 2 - 2 * ((u[3:nx, 1] - u[1:nx-2, ny-1]) / (2*dy) .* (v[2:nx-1, 1] - v[2:nx-1, ny-1]) / (2 * dx)) - ((v[3:nx, 1] - v[1:nx-2, 1]) / (2 * dy)).^2)))
-    println(b)
-end
+            p[i,j] = ((p[i+1, j] + p[i-1, j])*(dy^2) + (p[i, j+1] + p[i, j-1])*(dx^2))/(2*((dx^2)+(dy^2))) - (rho*(dx^2)*(dy^2)/(2*((dx^2) + (dy^2))))*(((u[i+1,j] - u[i-1, j])/2*dx + (v[i,j+1] - v[i, j-1])/2*dy)/dt-((u[i+1, j] - u[i-1, j])*(u[i+1, j]-u[i-1,j])/(4*dx^2))-(2*((u[i, j+1] - u[i, j-1])*(v[i+1,j] - v[i-1, j])/(4*dx*dy)))-((v[i,j+1]-v[i,j-1])*(v[i,j+1]-v[i, j-1])/(4*dy^2)))
 
-function pressure_poisson_periodic(p::Matrix{Float64}, dx::Float64, dy::Float64, nit::Int64, nx::Int64, ny::Int64)
 
-    Threads.@threads for j in range(1, stop=nit)
-    
-        pn = Matrix{Float64}(undef, nx, ny)
-        pn = p
-
-        p[2:nx-1, 2:ny-1] = (((pn[2:nx-1, 3:ny] + pn[2:nx-1, 1:ny-2]) * dy^2 + (pn[3:nx, 2:ny-1] + pn[1:nx-2, 2:ny-1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, 2:nx-1])
-        p[2:nx-1, ny-1] = (((pn[2:nx-1, 1] + pn[2:nx-1, ny-2])* dy^2 + (pn[3:nx, ny-1] + pn[1:nx-2, ny-1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, ny-1])
-        p[2:nx-1, 1] = (((pn[2:nx-1, 2] + pn[2:nx-1, ny-1])* dy^2 + (pn[3:nx, 1] + pn[1:ny-2, 1]) * dx^2) / (2 * (dx^2 + dy^2)) - dx^2 * dy^2 / (2 * (dx^2 + dy^2)) * b[2:nx-1, 1])
-        p[nx-1, :] =p[nx-2, :]
-        p[1, :] = p[2, :]
-
+        end
     end
 
-end
-
-function meshgrid(x, y)
-    X = [i for i in x, j in 1:length(y)]
-    Y = [j for i in 1:length(x), j in y]
-    return X, Y
 end
--- Step 12 Channel Flow/main.jl
+++ Step 12 Channel Flow/main.jl
@@ -30,47 +30,4 @@ un = zeros(nx, ny)
 vn = Matrix{Float64}(undef, nx, ny)
 vn = zeros(nx, ny)
 
-X, Y = meshgrid(x, y)
-
-udiff = 1
-stepcount = 0
-
-f = Figure(resolution = (800, 800))
-Axis(f[1, 1], backgroundcolor = "black")
-strength = vec(sqrt.(u .^ 2 .+ v .^ 2))
-
-frames = 1:0.01:10
-record(f, "Channel Flow.mp4", frames) do frame
-#while (udiff>0.001)
-    #if (udiff>0.001)
-    un = u
-    vn = v
-
-    build_up_b(rho, dt, dx, dy, u, v, b, nx, ny)
-    pressure_poisson_periodic(p, dx, dy, nit, nx, ny)
-
-    u[2:nx-1, 2:ny-1] = (un[2:nx-1, 2:ny-1] .- un[2:nx-1, 2:ny-1] * dt / dx * (un[2:nx-1, 2:ny-1] .- un[2:nx-1, 1:ny-2]) .- vn[2:nx-1, 2:ny-1] * dt / dy * (un[2:nx-1, 2:nx-1] .- un[1:nx-2, 2:ny-1]) .- dt / (2 * rho * dx) * (p[2:nx-1, 3:ny] .- p[2:nx-1, 1:ny-2]) .+ nu * (dt / dx^2 * (un[2:nx-1, 3:ny] .- 2 * un[2:nx-1, 2:ny-1] .+ un[2:nx-1, 1:ny-2]) .+ dt / dy^2 * (un[3:nx, 2:ny-1] .- 2 * un[2:nx-1, 2:ny-1] .+ un[1:nx-2, 2:ny-1])) .+ F * dt)
-    v[2:nx-1, 2:ny-1] = (vn[2:nx-1, 2:ny-1] .- un[2:nx-1, 2:ny-1] .* dt / dx .* (vn[2:nx-1, 2:ny-1] .- vn[2:nx-1, 1:ny-2]) .- vn[2:nx-1, 2:ny-1] .* dt / dy .* (vn[2:nx-1, 2:ny-1] .- vn[1:nx-2, 2:ny-1]) .- dt / (2 * rho * dy) .* (p[3:nx, 2:ny-1] - p[1:nx-2, 2:ny-1]) .+ nu .* (dt / dx^2 * (vn[2:nx-1, 3:ny] .- 2 .* vn[2:nx-1, 2:ny-1] .+ vn[2:nx-1, 1:ny-2]) .+ dt / dy^2 .* (vn[3:nx, 2:ny-1] .- 2 .* vn[2:nx-1, 2:ny-1] .+ vn[1:nx-2, 2:ny-1])))
-    u[2:nx-1, ny-1] = (un[2:nx-1, ny-1] - un[2:nx-1, ny-1] .* dt / dx .* (un[2:nx-1, ny-1] - un[2:nx-1, ny-2]) - vn[2:nx-1, ny-1] .* dt / dy .* (un[2:nx-1, ny-1] - un[1:nx-2, ny-1]) - dt / (2 * rho * dx) .* (p[2:nx-1, 1] - p[2:nx-1, ny-2]) .+ nu .* (dt / dx^2 * (un[2:nx-1, 1] - 2 * un[2:nx-1,ny-1] + un[2:nx-1, ny-2]) + dt / dy^2 .* (un[3:nx, ny-1] - 2 * un[2:nx-1, ny-1] + un[1:nx-2, ny-1])) .+ F .* dt)
-    u[2:nx-1, 1] = (un[2:nx-1, 1] - un[2:nx-1, 1] .* dt / dx .* (un[2:nx-1, 1] - un[2:nx-1, ny-1]) - vn[2:nx-1, 1] .* dt / dy .* (un[2:nx-1, 1] - un[1:nx-2, 1]) - dt / (2 * rho * dx) .* (p[2:nx-1, 2] - p[2:nx-1, ny-1]) .+ nu .* (dt / dx^2 * (un[2:nx-1, 2] - 2 * un[2:nx-1, 1] + un[2:nx-1, ny]) + dt / dy^2 .* (un[3:nx, 1] - 2 .* un[2:nx-1, 1] + un[1:nx-2, ny])) .+ F .* dt)
-    v[2:nx-1, ny-1] = (vn[2:nx-1, ny-1] - un[2:nx-1, ny-1] .* dt / dx .* (vn[2:nx-1, ny-1] - vn[2:nx-1, ny-2]) - vn[2:nx-1, ny-1] .* dt / dy .* (vn[2:nx-1, ny-1] - vn[1:nx-2, ny-1]) - dt / (2 * rho * dy) .* (p[3:nx, ny-1] - p[1:nx-2, ny-1]) .+ nu .* (dt / dx^2 .* (vn[2:nx-1, 1] - 2 .* vn[2:nx-1, ny-1] + vn[2:nx-1, ny-2]) + dt / dy^2 .* (vn[3:nx, ny-1] - 2 .* vn[2:nx-1, ny-1] + vn[1:nx-2, ny-1])))
-    v[2:nx-1, 1] = (vn[2:nx-1, 1] - un[2:nx-1, 1] .* dt / dx .* (vn[2:nx-1, 1] - vn[2:nx-1, ny-1]) - vn[2:nx-1, 1] .* dt / dy .* (vn[2:nx-1, 1] - vn[1:nx-2, 1]) - dt / (2 * rho * dy) .* (p[3:nx, 1] - p[1:nx-2, 1]) .+ nu .* (dt / dx^2 * (vn[2:nx-1, 2] - 2 .* vn[2:nx-1, 1] + vn[2:nx-1, ny-1]) + dt / dy^2 .* (vn[3:nx, 1] - 2 .* vn[2:nx-1, 1] + vn[1:nx-2, 1])))
-
-    u[1, :] .= 0
-    u[nx, :] .= 0
-    v[1, :] .= 0
-    v[nx, :] .= 0
-     
-    
-    #println(stepcount)
-    #udiff = (sum(u) - sum(un)) / sum(u)
-    #global stepcount += 1
-    #end
-    #contourf(x,y,p, xlims = (0,2), ylims = (0,2), c = cgrad(:ice))
-    #quiver!(X,Y, quiver= u,v , xlims = (0,2), ylims = (0,2), aspect_ratio=:equal, arrowscale=0.5)
-    arrows!(x, y, u, v, arrowsize = 10, lengthscale = 0.3,
-    arrowcolor = strength, linecolor = strength)
-    
-end
-
 
