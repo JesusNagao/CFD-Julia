025df598a160ffa1b49a33ba4f074d6ddb419801
Jesus Nagao <jnagaoalvarez@gmail.com>
2022-05-03 02:06:49 -0500

    Try PyCall

--- Step 12 Channel Flow/Operations.jl
+++ Step 12 Channel Flow/Operations.jl
@@ -1,12 +1,60 @@
-function pressure_equation()
+using PyCall
+py"""
 
-    for i in range(1, stop=nx)
-        for j in range(1, stop=ny)
+def build_up_b(rho, dt, dx, dy, u, v):
+    b = numpy.zeros_like(u)
+    b[1:-1, 1:-1] = (rho * (1 / dt * ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx) +
+                                    (v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) -
+                            ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx))**2 -
+                            2 * ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (2 * dy) *
+                                (v[1:-1, 2:] - v[1:-1, 0:-2]) / (2 * dx))-
+                            ((v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy))**2))
 
-            p[i,j] = ((p[i+1, j] + p[i-1, j])*(dy^2) + (p[i, j+1] + p[i, j-1])*(dx^2))/(2*((dx^2)+(dy^2))) - (rho*(dx^2)*(dy^2)/(2*((dx^2) + (dy^2))))*(((u[i+1,j] - u[i-1, j])/2*dx + (v[i,j+1] - v[i, j-1])/2*dy)/dt-((u[i+1, j] - u[i-1, j])*(u[i+1, j]-u[i-1,j])/(4*dx^2))-(2*((u[i, j+1] - u[i, j-1])*(v[i+1,j] - v[i-1, j])/(4*dx*dy)))-((v[i,j+1]-v[i,j-1])*(v[i,j+1]-v[i, j-1])/(4*dy^2)))
+    # Periodic BC Pressure @ x = 2
+    b[1:-1, -1] = (rho * (1 / dt * ((u[1:-1, 0] - u[1:-1,-2]) / (2 * dx) +
+                                    (v[2:, -1] - v[0:-2, -1]) / (2 * dy)) -
+                        ((u[1:-1, 0] - u[1:-1, -2]) / (2 * dx))**2 -
+                        2 * ((u[2:, -1] - u[0:-2, -1]) / (2 * dy) *
+                            (v[1:-1, 0] - v[1:-1, -2]) / (2 * dx)) -
+                        ((v[2:, -1] - v[0:-2, -1]) / (2 * dy))**2))
+
+    # Periodic BC Pressure @ x = 0
+    b[1:-1, 0] = (rho * (1 / dt * ((u[1:-1, 1] - u[1:-1, -1]) / (2 * dx) +
+                                (v[2:, 0] - v[0:-2, 0]) / (2 * dy)) -
+                        ((u[1:-1, 1] - u[1:-1, -1]) / (2 * dx))**2 -
+                        2 * ((u[2:, 0] - u[0:-2, 0]) / (2 * dy) *
+                            (v[1:-1, 1] - v[1:-1, -1]) / (2 * dx))-
+                        ((v[2:, 0] - v[0:-2, 0]) / (2 * dy))**2))
+
+    return b
+
+    def pressure_poisson_periodic(p, dx, dy):
+        pn = numpy.empty_like(p)
+        
+        for q in range(nit):
+            pn = p.copy()
+            p[1:-1, 1:-1] = (((pn[1:-1, 2:] + pn[1:-1, 0:-2]) * dy**2 +
+                            (pn[2:, 1:-1] + pn[0:-2, 1:-1]) * dx**2) /
+                            (2 * (dx**2 + dy**2)) -
+                            dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * b[1:-1, 1:-1])
+
+            # Periodic BC Pressure @ x = 2
+            p[1:-1, -1] = (((pn[1:-1, 0] + pn[1:-1, -2])* dy**2 +
+                            (pn[2:, -1] + pn[0:-2, -1]) * dx**2) /
+                        (2 * (dx**2 + dy**2)) -
+                        dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * b[1:-1, -1])
+
+            # Periodic BC Pressure @ x = 0
+            p[1:-1, 0] = (((pn[1:-1, 1] + pn[1:-1, -1])* dy**2 +
+                        (pn[2:, 0] + pn[0:-2, 0]) * dx**2) /
+                        (2 * (dx**2 + dy**2)) -
+                        dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * b[1:-1, 0])
+            
+            # Wall boundary conditions, pressure
+            p[-1, :] =p[-2, :]  # dp/dy = 0 at y = 2
+            p[0, :] = p[1, :]  # dp/dy = 0 at y = 0
+        
+        return p
 
 
-        end
-    end
-
-end
+"""
