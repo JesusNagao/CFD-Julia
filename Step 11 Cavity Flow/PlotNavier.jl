ce0f086a04dbc6d277c259724a4e819f729b9b27
ArathM <39510172+ArathM@users.noreply.github.com>
2022-05-17 08:57:08 -0500

    Add files via upload

--- Step 11 Cavity Flow/PlotNavier.jl
+++ Step 11 Cavity Flow/PlotNavier.jl
@@ -1,5 +1,6 @@
 using CairoMakie
 using LinearAlgebra
+using Colors
 
 #Correr una vez obtenidos los vectores u,v,p
 
@@ -9,7 +10,25 @@ x = LinRange(0,2,41)
 y = LinRange(0,2,41)
 leng = vec(norm.(Vec2f0.(u[:,:,1],v[:,:,1])))
 
-#CairoMakie.contourf(x, y, p[:,:,20], levels = 5, linewidth = 3)
-CairoMakie.arrows(x,y,u[:,:,50],v[:,:,50], arrowcolor = leng)
+fig, ax, cf = contour(x, y, p[:,:,1])
+ar = arrows!(x,y,u[:,:,1],v[:,:,1],lengthscale = 0.1, colormap = :blues)
+
+record(fig, "CavityFlow.mp4", 1:length(p[1,1,:])) do i
+
+    #leng = vec(norm.(Vec2f0.(u[:,:,i],v[:,:,i])))
+
+    cf[1] = x 
+    cf[2] = y 
+    cf[3] =  p[:,:,i] 
+
+    ar[1] = x
+    ar[2] = y
+    ar[3] = u[:,:,i]
+    ar[4] = v[:,:,i]
+
+    #xlims!(ax,[0,2]) 
+    #ylims!(ax,[0,2])
+
+end
 
 #CairoMakie.save("N2D.png", fig)
