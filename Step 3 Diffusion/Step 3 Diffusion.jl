6ceca9065fc6b44a83956a21a7150b1f5651cf66
Jesus Leobardo Nagao Alvarez <jesusleobardonagaoalvarez@Laptop-de-Jesus.local>
2026-04-29 17:03:15 -0600

    Update documentation for step 1 and 2

--- Step 3 Diffusion/Step 3 Diffusion.jl
+++ Step 3 Diffusion/Step 3 Diffusion.jl
@@ -2,23 +2,30 @@
 using Plots
 using Base.Threads
 using DelimitedFiles
+using BenchmarkTools
 
+#Diffusion equation
+#du/dt=vd^2u/dt^2
+
+#Create a structure that saves the information of the wave
 mutable struct Wave
     nx::Int64
     nt::Int64
     u::Array{Float64,2} 
 end
 
-#Método para inicializar el tipo solo con el número de puntos en x y en t
+#Instatiate the wave structure
 Wave(nx,nt) = Wave(nx,nt,zeros(nx,nt))      
 
+#Define the shape of our wave
 function Gaussian(x::Array{Float64,2},σ::Float64,μ::Float64)
     return [(1/sqrt(2*pi*σ^2)*exp(-((i-μ)^2)/(2*σ^2))) for i in x[:,1]]
 end
 
-#Método para cambiar el perfil inicial de la ola
+#Initialize wave
 function Initwave!(w::Wave,t::String)
 
+    #Use Gaussian distribution as the wave shape
     if t == "Gaussian"
         print("Escribe σ y μ en ese orden")
         σ::Float64 = parse(Float64, readline())
@@ -26,41 +33,51 @@ function Initwave!(w::Wave,t::String)
         w.u[:,1] = Gaussian(w.u,σ,μ)
     end
     
+    #Use Hat distribution as the wave shape
     if t == "Hat"
         w.u = ones(w.nx,w.nt)
     end
 
 end
 
-#Evolución temporal de la ola con la ecuación de difusión
+#Apply diffusion to the wave
 function Difussion(w::Wave; exportcsv = false)
-    nx::Int64 = w.nx
-    nt::Int64 = w.nt
-    ν::Float64 = 0.3
-    σ::Float64 = 0.2
-    dx::Float64 = 2/(nx-1)
-    dt::Float64 = σ*dx^2/ν
+    
+    #Initialize our variables
+    nx::Int64 = w.nx #number of steps in the spatial domain
+    nt::Int64 = w.nt #number of time steps
+    ν::Float64 = 0.3 #Parameter for our gaussian function
+    σ::Float64 = 0.2 #Parameter for our gaussian function
+    dx::Float64 = 2/(nx-1) #distance between grid points
+    dt::Float64 = σ*dx^2/ν #time step
+
 
     w.u[floor(Int64,0.5/dx):floor(Int64,1/ dx + 1),1] .= 2
 
+    #Create animation for diffusion for nt steps
     difusse = @animate for i in range(1,stop=nt-1)
 
+        #Run the discretized diffusion equation for the wave
+        #@threads is a macro that allows multithreading in julia
         @threads for j in range(2,stop = nx-1)
             w.u[j,i+1] = w.u[j,i] + ν*dt/dx^2*(w.u[j+1,i] - 2*w.u[j,i] + w.u[j-1,i])    
         end
         
+        #Plot the wave
         plot(LinRange(0,2,nx),w.u[:,i], xlabel = "u", ylabel = "x", title = "Difusión de una onda", xlims = (0,2), ylims = (0.5,3))
 
     end
-    
-    if exportcsv == true
-        writedlm("Diff.csv", w.u, ',')
-    end
 
+    #Exporting to CSV
+    #if exportcsv == true
+    #    writedlm("Diff.csv", w.u, ',')
+    #end
+    
+    
     gif(difusse, "dif.gif", fps = 1000)
 
 end
 
 dif = Wave(100,200)
 Initwave!(dif,"Hat")
-@time Difussion(dif,exportcsv = true) 
+@btime Difussion(dif,exportcsv = true) 
