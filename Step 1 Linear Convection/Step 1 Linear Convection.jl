231db0dac9d3f986279c0918dff704dc1eedf97c
Jesus Leobardo Nagao Alvarez <jesusleobardonagaoalvarez@Laptop-de-Jesus.local>
2026-04-29 00:34:04 -0600

    Update documentation for step 1 and 2

--- Step 1 Linear Convection/Step 1 Linear Convection.jl
+++ Step 1 Linear Convection/Step 1 Linear Convection.jl
@@ -1,36 +1,48 @@
 using Plots
 using BenchmarkTools
 
-const nx = 41
-const dx = 2 / (nx-1)
-const nt = 25
-const dt = 0.025
-const c = 1
+#Linear Convection Equation
+#du/dt+c*du/dx=0
+
+#Initialize variables
+const nx = 41 #number of steps in the spatial domain
+const dx = 2 / (nx-1) #distance between grid points
+const nt = 25 #number of time steps
+const dt = 0.025 #time step
+const c = 1 #wave propagation speed
+
 
 function run()
-
+    
+    #Initialize our wave with values of 1
     u = Array{Float64}(undef , nx);
     u = ones(nx)
 
+    #Create a hat pattern
     for i in range(Int(round(nx/3)), stop=Int(round(2*nx/3)))
 
         u[i] = 2;
 
     end
 
+    #Plot the initial state of the wave
     pb = plot(u)
 
+    #Apply convection for nt steps
     for i in range(1, stop=nt)
+        #Copy the previous state of the wave
         u_old = Array{Float64}(undef, nx)
         u_old = copy(u)
+        
         for j in range(2, stop=nx)
-
+            #Calculate the next state of the wave
             u[j] = u_old[j] - c * dt / dx * (u_old[j]- u_old[j-1])
 
         end
         
     end
 
+    #Plot the wave after nt time has passed
     pa = plot(u)
 
     plot(pb, pa, layout=(2,1))
--- Step 2 Nonlinear Convection/Nonlinear Convection.jl
+++ Step 2 Nonlinear Convection/Nonlinear Convection.jl
@@ -1,33 +1,51 @@
 using Plots
+using BenchmarkTools
 
-const nx = 41
-const dx = 2 / (nx-1)
-const nt = 25
-const dt = 0.025
-const c = 1
+#Non-linear Convection Equation
+#du/dt+u*du/dx=0
 
-u = Array{Float64}(undef , nx);
-u = ones(nx)
+#Initialize variables
+const nx = 61 #number of steps in the spatial domain
+const dx = 2 / (nx-1) #distance between grid points
+const nt = 25 #number of time steps
+const dt = 0.025 #time step
+const c = 1 #wave propagation speed
 
-for i in range(Int(round(nx/3)), stop=Int(round(2*nx/3)))
+function run()
 
-    u[i] = 2
+    #Initialize our wave with values of 1
+    u = Array{Float64}(undef , nx);
+    u = ones(nx)
 
-end
+    #Create a hat pattern
+    for i in range(Int(round(nx/3)), stop=Int(round(2*nx/3)))
 
-pb = plot(u)
-
-for i in range(1, stop=nt)
-    u_old = Array{Float64}(undef, nx)
-    u_old = copy(u)
-    for j in range(2, stop=nx)
-
-        u[j] = u_old[j] - u_old[j] * dt / dx * (u_old[j] - u_old[j-1])
+        u[i] = 2
 
     end
-    
+
+    #Plot the initial state of the wave
+    pb = plot(u, 0, 60)
+
+    #Apply convection for nt steps
+    for i in range(1, stop=nt)
+
+        #Copy the previous state of the wave
+        u_old = Array{Float64}(undef, nx)
+        u_old = copy(u)
+
+        for j in range(2, stop=nx)
+            #Calculate the next state of the wave
+            u[j] = u_old[j] - u_old[j] * dt / dx * (u_old[j] - u_old[j-1])
+
+        end
+        
+    end
+
+    #Plot the wave after nt time has passed
+    pa = plot(u, 0, 60)
+    plot(pb, pa, layout=(2,1))
+
 end
 
-pa = plot(u)
-
-plot(pb, pa, layout=(2,1))
+@btime run()
